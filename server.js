ee22a88c6e8280a023fe1ebd2141bd1a06b743db
Dongho Lee <dhlee5614@gmail.com>
2025-03-26 16:30:56 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+let corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+
+app.use(express.json());
+app.use(cors(corsOptions));
+//app.options('*', cors())
+app.get('/', (req, res) => {
+    // res.json({
+    //     success: true,
+    // });
+    console.log(req.method)
+    res.send({"message":"Hello World"})
+});
+app.get('/users', (req,res) => {
+    console.log(req.method)
+    res.json([
+        {
+            id: 1,
+            name: 'dong',
+            username: 'dong ho lee',
+            email: 'dhlee@ee.knu.ac.kr',
+            phone: "010 9366 7815"
+
+        },
+        {
+            id: 2,
+            name: 'kim',
+            username: 'nam cheol kim',
+            email: 'nckim@ee.knu.ac.kr',
+            phone: "010 1234 4567"
+
+        }
+    ])
+})
+
+app.post('/', (req, res) => {
+    console.log(req.method)
+    res.send({"message":`Post World ${req.body.user.name}`})
+    console.log(req.body)
+    console.log(req.query)
+})
+app.listen(port, () => {
+    console.log(`server is listening at localhost:${process.env.PORT}`);
+});
